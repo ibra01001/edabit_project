486cf4af723086d8837475976048f4a39850e7c9
REMILI Brahim <mohamedremili5000@gmail.com>
2026-06-12 19:20:27 +0100

    level hard2

--- main.js
+++ main.js
@@ -287,3 +287,34 @@ function sevenBoom(arr) {
   return "there is no 7 in the array";
 }
 
+/* exo 10
+There are three towers. The objective of the game is to move all the disks over to tower #3, but you can't place a larger disk onto a smaller disk. To play the game or learn more about the Tower of Hanoi, check the Resources tab.
+
+Tower of Hanoi
+
+Create a function that takes a number discs as an argument and returns the minimum amount of steps needed to complete the game.
+Examples
+
+towerHanoi(3) ➞ 7
+
+towerHanoi(5) ➞ 31
+
+towerHanoi(0) ➞ 0
+
+Notes
+
+    The amount of discs is always a positive integer.
+    1 disc can be changed per move.
+    */
+// Solution 10
+// level hard
+// points earned 40
+function towerHanoi(discs) {
+    let result = 1;
+
+    for (let i = 0; i < discs; i++) {
+        result = result * 2;
+    }
+
+    return result - 1;
+}
